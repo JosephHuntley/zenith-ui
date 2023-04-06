2e5757da10ac0bcec162920aa3c30b7c54dfe30d
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-04-06 13:59:51 -0400

    Implemented default 'style' variable

--- src/components/Card/CardStyles.ts
+++ src/components/Card/CardStyles.ts
@@ -4,8 +4,10 @@ import { HeaderProps } from "./CardHeader";
 import { BodyProps } from "./CardBody";
 import { FooterProps } from "./CardFooter";
 import { ZenithContextType } from "../../utils";
+import { style } from "../../utils/Zenith";
 
 export const Main = styled.div<CardProps & { zenith: ZenithContextType }>`
+  ${style}
   height: ${(props) =>
     props.size === "responsive"
       ? "none"
@@ -23,26 +25,7 @@ export const Main = styled.div<CardProps & { zenith: ZenithContextType }>`
       ? "30rem"
       : "23rem"};
 
-  font-size: ${(props) => props.font?.size || " "};
-  color: ${(props) => props.font?.color || " "};
-
-  // Margin
-  margin-left: ${(props) => props.margin?.left || props.margin?.x || ""};
-  margin-right: ${(props) => props.margin?.right || props.margin?.x || ""};
-  margin-top: ${(props) => props.margin?.top || props.margin?.y || ""};
-  margin-bottom: ${(props) => props.margin?.bottom || props.margin?.y || ""};
-  margin: ${(props) => props.margin?.m || ""};
-
-  //Padding
-  padding-left: ${(props) => props.padding?.left || props.padding?.x || ""};
-  padding-right: ${(props) => props.padding?.right || props.padding?.x || ""};
-  padding-top: ${(props) => props.padding?.top || props.padding?.y || ""};
-  padding-bottom: ${(props) => props.padding?.bottom || props.padding?.y || ""};
-  padding: ${(props) => props.padding?.p || ""};
-
-  border: 1px solid
-    ${(props) => props.color || props.zenith.colors.bg_secondary};
-  border-radius: ${(props) => props.radius || "0.7rem"};
+  
 
   display: grid;
   ${(props) =>
@@ -70,54 +53,15 @@ export const Main = styled.div<CardProps & { zenith: ZenithContextType }>`
 `;
 
 export const Header = styled.header<HeaderProps & { zenith: ZenithContextType }>`
-  width: ${(props) => props.size?.width || ""};
-  height: ${(props) => props.size?.height || ""};
-
+  
   font-size: ${(props) => props.font?.size || "3rem"};
-  color: ${(props) => props.font?.color || " "};
-
   background-color: ${(props) => props.color || "transparent"};
-
-  // Margin
-  margin-left: ${(props) => props.margin?.left || props.margin?.x || ""};
-  margin-right: ${(props) => props.margin?.right || props.margin?.x || ""};
-  margin-top: ${(props) => props.margin?.top || props.margin?.y || ""};
-  margin-bottom: ${(props) => props.margin?.bottom || props.margin?.y || ""};
-  margin: ${(props) => props.margin?.m || ""};
-
-  //Padding
-  padding-left: ${(props) => props.padding?.left || props.padding?.x || ""};
-  padding-right: ${(props) => props.padding?.right || props.padding?.x || ""};
-  padding-top: ${(props) => props.padding?.top || props.padding?.y || ""};
-  padding-bottom: ${(props) => props.padding?.bottom || props.padding?.y || ""};
   padding: ${(props) => props.padding?.p || "1rem"};
+  ${style}
 `;
 
 export const Body = styled.div<BodyProps & { zenith: ZenithContextType }>`
-  width: ${(props) => props.size?.width || ""};
-  height: ${(props) => props.size?.height || ""};
-
-  font-size: ${(props) => props.font?.size || " "};
-  color: ${(props) => props.font?.color || " "};
-
-  background-color: ${(props) => props.color || "transparent"};
-
-  // Margin
-  margin-left: ${(props) => props.margin?.left || props.margin?.x || ""};
-  margin-right: ${(props) => props.margin?.right || props.margin?.x || ""};
-  margin-top: ${(props) => props.margin?.top || props.margin?.y || ""};
-  margin-bottom: ${(props) => props.margin?.bottom || props.margin?.y || ""};
-  margin: ${(props) => props.margin?.m || ""};
-
-  //Padding
-  padding-left: ${(props) => props.padding?.left || props.padding?.x || ""};
-  padding-right: ${(props) => props.padding?.right || props.padding?.x || ""};
-  padding-top: ${(props) => props.padding?.top || props.padding?.y || ""};
-  padding-bottom: ${(props) => props.padding?.bottom || props.padding?.y || ""};
-  padding: ${(props) => props.padding?.p || ""};
-
-  border: 1px solid ${(props) => props.color || "none"};
-  border-radius: ${(props) => props.radius || "none"};
+  ${style}
 
   display: flex;
   align-items: flex-start;
@@ -125,34 +69,18 @@ export const Body = styled.div<BodyProps & { zenith: ZenithContextType }>`
 `;
 
 export const Footer = styled.footer<FooterProps & { zenith: ZenithContextType }>`
+${style}
   width: ${(props) => props.size?.width || `calc(100% - ${props.margin?.m || '0px'})`};
-  height: ${(props) => props.size?.height || ""};
-
+  
   font-size: ${(props) => props.font?.size || "2rem"};
-  color: ${(props) => props.font?.color || " "};
-
+  
   background-color: ${(props) =>
-    props.color || props.zenith.colors.bg_secondary};
+    props.color || 'transparent'};
 
-  // Margin
-  margin-left: ${(props) => props.margin?.left || props.margin?.x || ""};
-  margin-right: ${(props) => props.margin?.right || props.margin?.x || ""};
-  margin-top: ${(props) => props.margin?.top || props.margin?.y || ""};
-  margin-bottom: ${(props) => props.margin?.bottom || props.margin?.y || ""};
-  margin: ${(props) => props.margin?.m || ""};
-
-  //Padding
-  padding-left: ${(props) => props.padding?.left || props.padding?.x || ""};
-  padding-right: ${(props) => props.padding?.right || props.padding?.x || ""};
-  padding-top: ${(props) => props.padding?.top || props.padding?.y || ""};
-  padding-bottom: ${(props) => props.padding?.bottom || props.padding?.y || ""};
+  
   padding: ${(props) => props.padding?.p || "1.5rem"};
 
-  border: 1px solid
-    ${(props) => props.color || props.zenith.colors.bg_secondary};
-  border-radius: ${(props) => props.radius || ""};
-
-  gap: 0.5rem;
+  
   display: flex;
   flex-direction: column;
   justify-content: end;
--- src/components/Text/TextStyles.ts
+++ src/components/Text/TextStyles.ts
@@ -1,165 +1,35 @@
 import { ZenithContextType } from "../../utils";
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 import { TextProps } from "./Text";
+import {style} from '../../utils/Zenith'
+
+
+
 
 export const H1 = styled.h1<TextProps & { zenith: ZenithContextType }>`
-  width: ${(props) => props.size?.width};
-  font-size: ${(props) => props.font?.size};
-  color: ${(props) => props.font?.color || props.zenith.colors.text_primary};
-  text-align: ${(props) => props.align};
-  background-color: ${props => props.color || ''};
-
-  // Margin
-	margin-left: ${(props) => props.margin?.left || props.margin?.x || ''};
-	margin-right: ${(props) => props.margin?.right || props.margin?.x || ''};
-	margin-top: ${(props) => props.margin?.top || props.margin?.y || ''};
-	margin-bottom: ${(props) => props.margin?.bottom || props.margin?.y || ''};
-	margin: ${(props) => props.margin?.m || ''};
-
-	//Padding
-	padding-left: ${(props) => props.padding?.left || props.padding?.x || ''};
-	padding-right: ${(props) =>
-		props.padding?.right || props.padding?.x || ''};
-	padding-top: ${(props) => props.padding?.top || props.padding?.y || ''};
-	padding-bottom: ${(props) => props.padding?.bottom || props.padding?.y || ''};
-	padding: ${(props) => props.padding?.p || ''};
+${style}
 `;
 
 export const H2 = styled.h2<TextProps & { zenith: ZenithContextType }>`
-  width: ${(props) => props.size?.width || ""};
-  font-size: ${(props) => props.font?.size || ""};
-  color: ${(props) => props.font?.color || props.zenith.colors.text_primary};
-  text-align: ${(props) => props.align};
-  background-color: ${props => props.color || ''};
-
-  // Margin
-	margin-left: ${(props) => props.margin?.left || props.margin?.x || ''};
-	margin-right: ${(props) => props.margin?.right || props.margin?.x || ''};
-	margin-top: ${(props) => props.margin?.top || props.margin?.y || ''};
-	margin-bottom: ${(props) => props.margin?.bottom || props.margin?.y || ''};
-	margin: ${(props) => props.margin?.m || ''};
-
-	//Padding
-	padding-left: ${(props) => props.padding?.left || props.padding?.x || ''};
-	padding-right: ${(props) =>
-		props.padding?.right || props.padding?.x || ''};
-	padding-top: ${(props) => props.padding?.top || props.padding?.y || ''};
-	padding-bottom: ${(props) => props.padding?.bottom || props.padding?.y || ''};
-	padding: ${(props) => props.padding?.p || ''};
+ ${style}
 `;
 
 export const H3 = styled.h3<TextProps & { zenith: ZenithContextType }>`
-  width: ${(props) => props.size?.width};
-  font-size: ${(props) => props.font?.size};
-  color: ${(props) => props.font?.color || props.zenith.colors.text_primary};
-  text-align: ${(props) => props.align};
-  background-color: ${props => props.color || ''};
-
-  // Margin
-	margin-left: ${(props) => props.margin?.left || props.margin?.x || ''};
-	margin-right: ${(props) => props.margin?.right || props.margin?.x || ''};
-	margin-top: ${(props) => props.margin?.top || props.margin?.y || ''};
-	margin-bottom: ${(props) => props.margin?.bottom || props.margin?.y || ''};
-	margin: ${(props) => props.margin?.m || ''};
-
-	//Padding
-	padding-left: ${(props) => props.padding?.left || props.padding?.x || ''};
-	padding-right: ${(props) =>
-		props.padding?.right || props.padding?.x || ''};
-	padding-top: ${(props) => props.padding?.top || props.padding?.y || ''};
-	padding-bottom: ${(props) => props.padding?.bottom || props.padding?.y || ''};
-	padding: ${(props) => props.padding?.p || ''};
+  ${style}
 `;
 
 export const H4 = styled.h4<TextProps & { zenith: ZenithContextType }>`
-  width: ${(props) => props.size?.width};
-  font-size: ${(props) => props.font?.size};
-  color: ${(props) => props.font?.color || props.zenith.colors.text_primary};
-  text-align: ${(props) => props.align};
-  background-color: ${props => props.color || ''};
-
-  // Margin
-	margin-left: ${(props) => props.margin?.left || props.margin?.x || ''};
-	margin-right: ${(props) => props.margin?.right || props.margin?.x || ''};
-	margin-top: ${(props) => props.margin?.top || props.margin?.y || ''};
-	margin-bottom: ${(props) => props.margin?.bottom || props.margin?.y || ''};
-	margin: ${(props) => props.margin?.m || ''};
-
-	//Padding
-	padding-left: ${(props) => props.padding?.left || props.padding?.x || ''};
-	padding-right: ${(props) =>
-		props.padding?.right || props.padding?.x || ''};
-	padding-top: ${(props) => props.padding?.top || props.padding?.y || ''};
-	padding-bottom: ${(props) => props.padding?.bottom || props.padding?.y || ''};
-	padding: ${(props) => props.padding?.p || ''};
+ ${style}
 `;
 
 export const H5 = styled.h5<TextProps & { zenith: ZenithContextType }>`
-  width: ${(props) => props.size?.width};
-  font-size: ${(props) => props.font?.size};
-  color: ${(props) => props.font?.color || props.zenith.colors.text_primary};
-  text-align: ${(props) => props.align};
-
-  // Margin
-	margin-left: ${(props) => props.margin?.left || props.margin?.x || ''};
-	margin-right: ${(props) => props.margin?.right || props.margin?.x || ''};
-	margin-top: ${(props) => props.margin?.top || props.margin?.y || ''};
-	margin-bottom: ${(props) => props.margin?.bottom || props.margin?.y || ''};
-	margin: ${(props) => props.margin?.m || ''};
-	background-color: ${props => props.color || ''};
-
-	//Padding
-	padding-left: ${(props) => props.padding?.left || props.padding?.x || ''};
-	padding-right: ${(props) =>
-		props.padding?.right || props.padding?.x || ''};
-	padding-top: ${(props) => props.padding?.top || props.padding?.y || ''};
-	padding-bottom: ${(props) => props.padding?.bottom || props.padding?.y || ''};
-	padding: ${(props) => props.padding?.p || ''};
+  ${style}
 `;
 
 export const H6 = styled.h6<TextProps & { zenith: ZenithContextType }>`
-  width: ${(props) => props.size?.width};
-  font-size: ${(props) => props.font?.size};
-  color: ${(props) => props.font?.color || props.zenith.colors.text_primary};
-  text-align: ${(props) => props.align};
-  background-color: ${props => props.color || ''};
-
-  // Margin
-	margin-left: ${(props) => props.margin?.left || props.margin?.x || ''};
-	margin-right: ${(props) => props.margin?.right || props.margin?.x || ''};
-	margin-top: ${(props) => props.margin?.top || props.margin?.y || ''};
-	margin-bottom: ${(props) => props.margin?.bottom || props.margin?.y || ''};
-	margin: ${(props) => props.margin?.m || ''};
-
-	//Padding
-	padding-left: ${(props) => props.padding?.left || props.padding?.x || ''};
-	padding-right: ${(props) =>
-		props.padding?.right || props.padding?.x || ''};
-	padding-top: ${(props) => props.padding?.top || props.padding?.y || ''};
-	padding-bottom: ${(props) => props.padding?.bottom || props.padding?.y || ''};
-	padding: ${(props) => props.padding?.p || ''};
+   ${style}
 `;
 
 export const P = styled.p<TextProps & { zenith: ZenithContextType }>`
-  width: ${(props) => props.size?.width};
-  font-size: ${(props) => props.font?.size};
-  color: ${(props) => props.font?.color || props.zenith.colors.text_primary};
-  text-align: ${(props) => props.align};
-
-  background-color: ${props => props.color || ''};
-
-  // Margin
-	margin-left: ${(props) => props.margin?.left || props.margin?.x || ''};
-	margin-right: ${(props) => props.margin?.right || props.margin?.x || ''};
-	margin-top: ${(props) => props.margin?.top || props.margin?.y || ''};
-	margin-bottom: ${(props) => props.margin?.bottom || props.margin?.y || ''};
-	margin: ${(props) => props.margin?.m || ''};
-
-	//Padding
-	padding-left: ${(props) => props.padding?.left || props.padding?.x || ''};
-	padding-right: ${(props) =>
-		props.padding?.right || props.padding?.x || ''};
-	padding-top: ${(props) => props.padding?.top || props.padding?.y || ''};
-	padding-bottom: ${(props) => props.padding?.bottom || props.padding?.y || ''};
-	padding: ${(props) => props.padding?.p || ''};
+ ${style}
 `;
