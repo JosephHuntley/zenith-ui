25554a0210246ba086710932dc25ff11bfd3bd9b
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-04-06 16:47:17 -0400

    Updated CloseButton

--- src/components/CloseButton/CloseButton.tsx
+++ src/components/CloseButton/CloseButton.tsx
@@ -1,39 +1,28 @@
-import { ZenithContext, ZenithContextType } from "../../utils"
+import {
+  ZenithContext,
+  ZenithContextType,
+  ZenithProps,
+  style,
+} from "../../utils"
 import React, { useContext } from "react"
 import styled from "styled-components"
 
-export type props = {
-  size?: string
-  variant?: "lg" | "md" | "sm" | string
-  color?: string
-  onClick?: () => any
-}
+export type CloseProps = ZenithProps & {}
 
-const ClsButton = styled.span<props & { zenith: ZenithContextType }>`
-  color: ${(props) => props.color || props.zenith.colors.white};
-  font-weight: bold;
-  font-size: ${(props) => props.size || "2rem"};
-  line-height: ${(props) => props.size || "2rem"};
+const ClsButton = styled.span<CloseProps & { zenith: ZenithContextType }>`
+  ${style}
+  color: ${(props) => props.font?.color || props.zenith.colors.white};
+  background-color: ${(props) => props.color || "transparent"};
+  font-size: ${(props) => props.size?.width || "2rem"};
+
   cursor: pointer;
   transition: 0.3s;
 `
 
-const CloseButton = ({ size, color, onClick }: props) => {
+const CloseButton = (props: CloseProps) => {
   const zenith = useContext(ZenithContext)
-
-  switch (size) {
-    case "lg":
-      size = "5rem"
-      break
-    case "md":
-      size = "2.5rem"
-      break
-    case "sm":
-      size = "1rem"
-      break
-  }
   return (
-    <ClsButton zenith={zenith} color={color} size={size} onClick={onClick}>
+    <ClsButton zenith={zenith} {...props}>
       X
     </ClsButton>
   )
