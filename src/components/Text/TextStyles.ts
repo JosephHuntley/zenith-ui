b648e171cbff5a1164a99247b8598dec157be7bd
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-04-06 20:47:38 -0400

    Added default Transparent BG

--- src/components/Text/TextStyles.ts
+++ src/components/Text/TextStyles.ts
@@ -8,28 +8,35 @@ import {style} from '../../utils/Zenith'
 
 export const H1 = styled.h1<TextProps & { zenith: ZenithContextType }>`
 ${style}
+background-color: ${props => props.color || 'transparent'};
 `;
 
 export const H2 = styled.h2<TextProps & { zenith: ZenithContextType }>`
  ${style}
+ background-color: ${props => props.color || 'transparent'};
 `;
 
 export const H3 = styled.h3<TextProps & { zenith: ZenithContextType }>`
   ${style}
+  background-color: ${props => props.color || 'transparent'};
 `;
 
 export const H4 = styled.h4<TextProps & { zenith: ZenithContextType }>`
  ${style}
+ background-color: ${props => props.color || 'transparent'};
 `;
 
 export const H5 = styled.h5<TextProps & { zenith: ZenithContextType }>`
   ${style}
+  background-color: ${props => props.color || 'transparent'};
 `;
 
 export const H6 = styled.h6<TextProps & { zenith: ZenithContextType }>`
    ${style}
+   background-color: ${props => props.color || 'transparent'};
 `;
 
 export const P = styled.p<TextProps & { zenith: ZenithContextType }>`
  ${style}
+ background-color: ${props => props.color || 'transparent'};
 `;
