dddb5718ea45a6a300a6430e2f4c6aaf6a882de5
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-04-13 16:04:06 -0400

    Fixed Formatting Error with Styles

--- src/components/Badge/BadgeStyles.ts
+++ src/components/Badge/BadgeStyles.ts
@@ -1,20 +1,15 @@
 import styled from "styled-components";
 import { BadgeProps } from "./Badge";
-import { ZenithContextType } from "../../utils";
+import { ZenithContextType, style } from "../../utils";
 
-export const BadgeMain = styled.span<
-  BadgeProps & { zenith: ZenithContextType }
->`
-  width: ${(props) => props.size?.width || ""};
-  height: ${(props) => props.size?.height || ""};
-
+export const BadgeMain = styled.span<BadgeProps & { zenith: ZenithContextType }>`
+${style}
+ 
   font-size: ${(props) => props.font?.size || "1rem"};
   color: ${(props) =>
     props.font?.color || props.zenith.status[props.status].font};
 
-  // If the user has a props.color set then that is the BG color.
-  // Else if the variant is outline or hover outline the BG is transparent
-  // Else the color is the value of bg_secondary
+  
   background-color: ${(props) =>
     props.variant === "outline" || props.variant === "hover-outline"
       ? "transparent"
@@ -28,23 +23,16 @@ export const BadgeMain = styled.span<
   align-items: center;
   text-align: center;
 
+  border-radius: ${props => props.radius || '.3rem'};
+
   // Margin
   margin: ${(props) => props.margin?.m || "1rem"};
-  margin-left: ${(props) => props.margin?.left || props.margin?.x || ""};
-  margin-right: ${(props) => props.margin?.right || props.margin?.x || ""};
-  margin-top: ${(props) => props.margin?.top || props.margin?.y || ""};
-  margin-bottom: ${(props) => props.margin?.bottom || props.margin?.y || ""};
 
   //Padding
   padding: ${(props) => props.padding?.p || ".3rem"};
-  padding-left: ${(props) => props.padding?.left || props.padding?.x || ""};
-  padding-right: ${(props) => props.padding?.right || props.padding?.x || ""};
-  padding-top: ${(props) => props.padding?.top || props.padding?.y || ""};
-  padding-bottom: ${(props) => props.padding?.bottom || props.padding?.y || ""};
 
   border: 1px solid
     ${(props) => props.color || props.zenith.status[props.status].bg};
-  border-radius: ${(props) => props.radius || "0.3rem"};
   box-shadow: ${(props) =>
     props.variant === "elevated" ? "0px 4px 4px rgba(0, 0, 0, 0.25)" : "none"};
 
